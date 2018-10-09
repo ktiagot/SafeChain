b5aa9409acdd46a2eca9951cea4a50c4c0431332
unknown <tiagocalutis@hotmail.com.br>
2018-10-09 09:39:43 -0300

    Tela de cadastro do organizador

---
+++ src/pages/cadastro-organizador/cadastro-organizador.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { LoginPage } from '../login/login';
+import { CadastroPage } from '../cadastro/cadastro';
+import { CadastroParticipantePage } from '../cadastro-participante/cadastro-participante';
+
+@Component({
+  selector: 'page-cadastro-organizador',
+  templateUrl: 'cadastro-organizador.html'
+})
+export class CadastroOrganizadorPage {
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  constructor(public navCtrl: NavController) {
+  }
+  goToHome(params){
+    if (!params) params = {};
+    this.navCtrl.push(HomePage);
+  }goToLogin(params){
+    if (!params) params = {};
+    this.navCtrl.push(LoginPage);
+  }goToCadastro(params){
+    if (!params) params = {};
+    this.navCtrl.push(CadastroPage);
+  }goToCadastroParticipante(params){
+    if (!params) params = {};
+    this.navCtrl.push(CadastroParticipantePage);
+  }goToCadastroOrganizador(params){
+    if (!params) params = {};
+    this.navCtrl.push(CadastroOrganizadorPage);
+  }
+}
